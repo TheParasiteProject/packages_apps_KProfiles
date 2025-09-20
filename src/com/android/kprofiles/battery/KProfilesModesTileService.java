5a94cf28739c5deeca9691cd48b861f4d1a3e238
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-09-20 13:49:56 +0900

    KProfiles: Change tile icon based on mode
    * Make it more recognizable
    
    Change-Id: I48390129a804fef54f052872485d17980fece341

--- src/com/android/kprofiles/battery/KProfilesModesTileService.java
+++ src/com/android/kprofiles/battery/KProfilesModesTileService.java
@@ -3,6 +3,7 @@ package com.android.kprofiles.battery;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Icon;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
@@ -13,6 +14,11 @@ public class KProfilesModesTileService extends TileService {
 
     private Context mContext;
     private boolean mSelfChange = false;
+
+    private Icon mNoneIcon;
+    private Icon mBatteryIcon;
+    private Icon mBalancedIcon;
+    private Icon mPerformanceIcon;
 
     private final BroadcastReceiver mServiceStateReceiver =
             new BroadcastReceiver() {
@@ -33,8 +39,13 @@ public class KProfilesModesTileService extends TileService {
         mContext = getApplicationContext();
         if (Utils.isModesSupported(mContext)) {
             super.onCreate();
+            mNoneIcon = Icon.createWithResource(mContext, R.drawable.ic_kprofiles);
+            mBatteryIcon = Icon.createWithResource(mContext, R.drawable.ic_battery);
+            mBalancedIcon = Icon.createWithResource(mContext, R.drawable.ic_balanced);
+            mPerformanceIcon = Icon.createWithResource(mContext, R.drawable.ic_performance);
             return;
         }
+
         Tile tile = getQsTile();
         tile.setState(Tile.STATE_UNAVAILABLE);
         tile.updateTile();
@@ -128,15 +139,19 @@ public class KProfilesModesTileService extends TileService {
                         : Tile.STATE_INACTIVE);
 
         if (mode.equals(getString(R.string.kprofiles_modes_value_none))) {
+            tile.setIcon(mNoneIcon);
             tile.setContentDescription(getString(R.string.kprofiles_modes_none));
             tile.setSubtitle(getString(R.string.kprofiles_modes_none));
         } else if (mode.equals(getString(R.string.kprofiles_modes_value_battery))) {
+            tile.setIcon(mBatteryIcon);
             tile.setContentDescription(getString(R.string.kprofiles_modes_battery));
             tile.setSubtitle(getString(R.string.kprofiles_modes_battery));
         } else if (mode.equals(getString(R.string.kprofiles_modes_value_balanced))) {
+            tile.setIcon(mBalancedIcon);
             tile.setContentDescription(getString(R.string.kprofiles_modes_balanced));
             tile.setSubtitle(getString(R.string.kprofiles_modes_balanced));
         } else if (mode.equals(getString(R.string.kprofiles_modes_value_performance))) {
+            tile.setIcon(mPerformanceIcon);
             tile.setContentDescription(getString(R.string.kprofiles_modes_performance));
             tile.setSubtitle(getString(R.string.kprofiles_modes_performance));
         }
